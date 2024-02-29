4884769fe21f9730772af9c2bcfee7e09f9606ea
Jfrattin <106035011+Jfrattin@users.noreply.github.com>
2024-02-29 08:20:57 -0300

    Cambios de algunos comentarios

--- Problema3/modules/grafo.py
+++ Problema3/modules/grafo.py
@@ -35,16 +35,16 @@ class Grafo:
     def __iter__(self):#Permite iteraar sobre los objetos de vértice en el grafo.
         return iter(self.listaVertices.values())
 
-    ## tomaremos como distancia el peso maximo
+    ## tomaremos como peso el peso maximo
     def maximo_peso_transportado(self, ciudad_inicio, ciudad_destino):
         cola_prioridad = Monticulo()
-        pesosmax = {ciudad: float("-inf") for ciudad in self.listaVertices}  # Asigna a cada ciudad distancia inicial de menos infinito, excepto la ciudad de inicio
+        pesosmax = {ciudad: float("-inf") for ciudad in self.listaVertices}  # Asigna a cada ciudad peso inicial de menos infinito, excepto la ciudad de inicio
         pesosmax[ciudad_inicio] = float("inf")
         cola_prioridad.insertar(ciudad_inicio, float("inf"))
 
         while not cola_prioridad.esta_vacia():  # Mientras no esté vacía
-            ciudad_actual = cola_prioridad.obtener()  # Obtiene la ciudad con la mayor distancia
-            if ciudad_actual == ciudad_destino:  # Si es la ciudad final devuelve la distancia
+            ciudad_actual = cola_prioridad.obtener()  # Obtiene la ciudad con la mayor 
+            if ciudad_actual == ciudad_destino:  # Si es la ciudad final devuelve la peso
                 return pesosmax[ciudad_actual]
 
             vertice_actual = self.listaVertices[ciudad_actual]
@@ -52,9 +52,9 @@ class Grafo:
             for vecino in vertice_actual.obtenerConexiones():  # Para cada vecino de la ciudad actual
                 ciudad_vecino = vecino.id
                 peso = vertice_actual.obtenerPonderacion(vecino)
-                peso_posible = min(pesosmax[ciudad_actual], peso)  # Calcula el posible peso como el mínimo entre la distancia actual a la ciudad y el peso de la arista a ese vecino
-                if peso_posible > pesosmax[ciudad_vecino]:  # Si este peso posible es mayor que la distancia actual al vecino
-                    pesosmax[ciudad_vecino] = peso_posible  # Actualiza la distancia del vecino
+                peso_posible = min(pesosmax[ciudad_actual], peso)  # Calcula el posible peso como el mínimo entre la peso actual a la ciudad y el peso de la arista a ese vecino
+                if peso_posible > pesosmax[ciudad_vecino]:  # Si este peso posible es mayor que la peso actual al vecino
+                    pesosmax[ciudad_vecino] = peso_posible  # Actualiza la peso del vecino
                     cola_prioridad.insertar(ciudad_vecino, peso_posible)  # Lo inserta en la cola de prioridad
         
         return float("-inf")
