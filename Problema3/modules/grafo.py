931c1b9ced774ef11135c70049598ce08faeaa3b
Joaquin <106035011+Jfrattin@users.noreply.github.com>
2024-07-03 09:10:36 -0300

    Add files via upload
    modifico grafos

--- Problema3/modules/grafo.py
+++ Problema3/modules/grafo.py
@@ -35,7 +35,7 @@ class Grafo:
     def __iter__(self):#Permite iteraar sobre los objetos de vértice en el grafo.
         return iter(self.listaVertices.values())
 
-    ## tomaremos como peso el peso maximo
+    ## tomaremos como "distancia" el peso maximo
     def maximo_peso_transportado(self, ciudad_inicio, ciudad_destino):
         cola_prioridad = Monticulo()
         pesosmax = {ciudad: float("-inf") for ciudad in self.listaVertices}  # Asigna a cada ciudad peso inicial de menos infinito, excepto la ciudad de inicio
@@ -43,8 +43,10 @@ class Grafo:
         cola_prioridad.insertar(ciudad_inicio, float("inf"))
 
         while not cola_prioridad.esta_vacia():  # Mientras no esté vacía
-            ciudad_actual = cola_prioridad.obtener()  # Obtiene la ciudad con la mayor 
-            if ciudad_actual == ciudad_destino:  # Si es la ciudad final devuelve la peso
+            ciudad_actual = cola_prioridad.obtener()  # Obtiene la ciudad con la mayor distancia es decir la ciudad con mayor peso maximo
+            # print(cola_prioridad)
+            # print(ciudad_actual)
+            if ciudad_actual == ciudad_destino:  # Si es la ciudad final devuelve el peso maximo .
                 return pesosmax[ciudad_actual]
 
             vertice_actual = self.listaVertices[ciudad_actual]
@@ -54,28 +56,39 @@ class Grafo:
                 peso = vertice_actual.obtenerPonderacion(vecino)
                 peso_posible = min(pesosmax[ciudad_actual], peso)  # Calcula el posible peso como el mínimo entre la peso actual a la ciudad y el peso de la arista a ese vecino
                 if peso_posible > pesosmax[ciudad_vecino]:  # Si este peso posible es mayor que la peso actual al vecino
-                    pesosmax[ciudad_vecino] = peso_posible  # Actualiza la peso del vecino
+                    pesosmax[ciudad_vecino] = peso_posible  # Actualiza lel peso del vecino
                     cola_prioridad.insertar(ciudad_vecino, peso_posible)  # Lo inserta en la cola de prioridad
-        
-        return float("-inf")
-    
-    def minimo_precio_envio(self, ciudad_inicio, ciudad_destino):
-        cola = Monticulo()
-        costos = {city: float("inf") for city in self.listaVertices}#Asigna a cada ciudad un costo inicial infinito
-        costos[ciudad_inicio] = 0 #Ciudad de inicio tiene un costo de cero
-        cola.insertar(ciudad_inicio, 0)
 
-        
-        while not cola.esta_vacia():#Mientras la cola no esté vacía
-            ciudad_actual = cola.obtener()#Obtiene la ciudad con el menor costo
-            if ciudad_actual == ciudad_destino:#Si es la ciudad final, devuelve el costo
-                return costos[ciudad_actual]
+        return float("-inf")
+
+    def minimo_precio_envio(self, ciudad_inicio, ciudad_destino):
+        pesomax = self.maximo_peso_transportado(ciudad_inicio, ciudad_destino) # calculo el maximo peso posible a trasportar
+        cola_prioridad = Monticulo()
+        costos = {city: float("inf") for city in self.listaVertices}  # Asigna a cada ciudad un costo inicial infinito
+        costos[ciudad_inicio] = 0  # Ciudad de inicio tiene un costo de cero
+        cola_prioridad.insertar(ciudad_inicio, 0)
+
+        while not cola_prioridad.esta_vacia():
+            ciudad_actual = cola_prioridad.obtener()  # Obtiene la ciudad con el menor costo actual
+
+            if ciudad_actual == ciudad_destino:
+                return costos[ciudad_actual]  # Si es la ciudad final, devuelve el costo mínimo hasta ella
 
             vertice_actual = self.listaVertices[ciudad_actual]
-            for vecino in vertice_actual.obtenerConexiones():#Para cada vecino de la ciudad actual
+            for vecino in vertice_actual.obtenerConexiones():  # Para cada vecino de la ciudad actual
                 ciudad_vecino = vecino.id
-                costo = vertice_actual.obtenerPrecio(vecino)  #devuelve el precio de la arista
+                costo = vertice_actual.obtenerPrecio(vecino)  # Obtiene el precio de la arista
+                peso = vertice_actual.obtenerPonderacion(vecino) # obtengo el peso 
                 costo_posible = costos[ciudad_actual] + costo
-                if costo_posible < costos[ciudad_vecino]:#Si el posible costo es menor que el costo actual al vecino 
-                    costos[ciudad_vecino] = costo_posible#Actualiza el costo del vecino
-                    cola.insertar(ciudad_vecino, -costo_posible)#Lo inserta en la cola de prioridad
+                # print("###ciudad_actual##")
+                # print(ciudad_actual)
+                # print(pesomax)
+                # print(peso)
+                # print("#####")
+                if pesomax <= peso: # siempre que peso maximo sea menor al peso trasportado
+                    if costo_posible < costos[ciudad_vecino]:
+                                    # Si el costo posible es menor que el costo actual y el peso de la arista es <= pesomax
+                                        costos[ciudad_vecino] = costo_posible  # Actualiza el costo del vecino
+                                        cola_prioridad.insertar(ciudad_vecino, -costo_posible)  # Inserta en la cola de prioridad
+
+        return float("inf")  # Si no se encontró camino válido, devuelve infinito o maneja según tu lógica
